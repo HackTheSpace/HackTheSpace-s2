048601b2d8ec406241b4b113f796226df760414c
K Harsh <harshnaidu2u@gmail.com>
2024-07-17 22:20:06 +0530

    FAQs section added

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,8 @@
 import AboutUs from "@/components/AboutUs";
 import HeroSection from "@/components/HeroSection";
 import Navbar from "@/components/Navbar";
-<<<<<<< HEAD
-import FAQsSection from "@/components/FAQs";
-=======
 import WhatIsHTS from "@/components/WhatIsHTS";
->>>>>>> 5c20e27418fe7749f6bc38a5cdcf6f5471d8bb3f
+import FAQsSection from "@/components/FAQs";
 
 export default function Home() {
   return (
@@ -13,11 +10,8 @@ export default function Home() {
       <Navbar />
       <HeroSection />
       <AboutUs />
-<<<<<<< HEAD
-      <FAQsSection />
-=======
       <WhatIsHTS />
->>>>>>> 5c20e27418fe7749f6bc38a5cdcf6f5471d8bb3f
+      <FAQsSection />
     </main>
   );
 }
