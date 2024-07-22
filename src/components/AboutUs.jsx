d212b93d1e2e6dd813e34c9b97f4817a000507ac
Harsh Gupta <harshgupta3341@gmail.com>
2024-07-23 03:51:00 +0530

    fixed herosection image

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import Heading from "./Heading";
 import Paragraph from "./Paragraph";
+import Image from "next/image";
 
 const AboutUs = () => {
   return (
     <section className="AboutUsDiv" id="aboutUS">
+      <Image
+        src="/astronaut.svg"
+        alt="Astronaut"
+        width={500}
+        height={500}
+        className="hero-section__image"
+      />
       <h1 className="heading1">ABOUT</h1>
       <Heading className="heading2">ABOUT US</Heading>
 
@@ -12,12 +20,22 @@ const AboutUs = () => {
 
       <div className="ParaDiv2">
         <Paragraph>
-        We are active leaders &amp; passionate community builders who work towards a collective goal of strengthening the region to get all the tech enthusiasts and career aspirants equal opportunities and exposure as people in metro cities or all around the world get! We are determined to cultivate an ambiance where individuals can coact to creat an inclusive environment to devise, grasp and develop solutions. Our motive is not only concerned with technical but also cultural development. As a team of aspirants we welcome growth as a part of our journey.
+          We are active leaders &amp; passionate community builders who work
+          towards a collective goal of strengthening the region to get all the
+          tech enthusiasts and career aspirants equal opportunities and exposure
+          as people in metro cities or all around the world get! We are
+          determined to cultivate an ambiance where individuals can coact to
+          creat an inclusive environment to devise, grasp and develop solutions.
+          Our motive is not only concerned with technical but also cultural
+          development. As a team of aspirants we welcome growth as a part of our
+          journey.
         </Paragraph>
       </div>
       <div className="ParaDiv2">
         <Paragraph>
-        So, here we are, organizing the biggest hackathon of central India, with enormous amounts of opportunities and capabilities to level up the community reach of the region.
+          So, here we are, organizing the biggest hackathon of central India,
+          with enormous amounts of opportunities and capabilities to level up
+          the community reach of the region.
         </Paragraph>
       </div>
     </section>
