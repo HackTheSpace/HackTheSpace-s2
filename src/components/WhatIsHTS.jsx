e00eac73073732ff976f49a5dd4c33246b0b763e
Harsh Gupta <harshgupta3341@gmail.com>
2024-07-23 02:19:50 +0530

    changed design and date

--- src/components/WhatIsHTS.jsx
+++ src/components/WhatIsHTS.jsx
@@ -60,7 +60,7 @@ const WhatIsHTS = () => {
           </div>
           <div className="whatIs-text__para">
             <Paragraph className="whatIs-text__para1">
-            HackTheSpace is an exciting 24-hour event that takes place in Bhilai,India. It is a time-limited event that encourages participants to brainstorm, develop, and present their ideas to a panel of judges. This event is designed to foster creativity, collaboration, and innovation among participants. It offers an excellent opportunity for participants to showcase their skills, network with peers, and potentially win prizes and recognition for their work.
+            HackTheSpace is an exciting 24-hour event that takes place in Bhilai, India. It is a time-limited event that encourages participants to brainstorm, develop, and present their ideas to a panel of judges. This event is designed to foster creativity, collaboration, and innovation among participants. It offers an excellent opportunity for participants to showcase their skills, network with peers, and potentially win prizes and recognition for their work.
             </Paragraph>
             <Paragraph className="whatIs-text__para2">
             During the event, participants will work in teams to develop innovative ideas that can solve real-world problems. They will be encouraged to use their creativity, technical skills, and critical thinking abilities to develop solutions that are both practical and effective.
