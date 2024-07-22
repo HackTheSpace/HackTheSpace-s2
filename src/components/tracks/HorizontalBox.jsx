a768685378ac48363f32d3b541ca18dce0b35999
girishsinha <gireeshsinha06@gmail.com>
2024-07-22 19:37:49 +0530

    edit tracks, date

--- src/components/tracks/HorizontalBox.jsx
+++ src/components/tracks/HorizontalBox.jsx
@@ -1,59 +1,56 @@
 import React, { useRef } from "react";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion, AnimatePresence, useInView } from "framer-motion";
 import Image from "next/image";
 
-const HorizontalBox = ({ title1, title2, inView, img1, img2 }) => {
-  return (
-    <AnimatePresence>
-      {inView && (
-        <div className="horizontal-container">
-          <motion.div
-            key="1"
-            initial={{ rotate: 90 }}
-            animate={{ rotate: 0, transition: { duration: 0.25, delay: 0.75 } }}
-            className="horizontal-box tracks-box "
-            style={{ transformOrigin: " top left", zIndex: 3 }}
-          >
-            <Image
-              src={img1}
-              alt="IOT"
-              width={80}
-              height={80}
-              className="icon"
-            />
-            <h3 className="text">{title1}</h3>
-          </motion.div>
-          <motion.div
-            key="2"
-            initial={{ y: "-220%", rotate: -10 }}
-            animate={{
-              y: "0%",
-              rotate: 0,
+const HorizontalBox = ({ title1, title2, img1, img2 }) => {
+  const ref = useRef(null);
+  const inView = useInView(ref);
+  console.log(inView);
+  const animation = {
+    visible: {
+      height: "46%",
+      transition: { duration: 0.5, delay: 0.25 },
+    },
+    rotate: {
+      y: "0%",
+      rotate: 0,
 
-              transition: {
-                duration: 0.3,
-                delay: 1,
-                type: "spring",
-                stiffness: 500,
-                damping: 30,
-                mass: 1,
-              },
-            }}
-            className="horizontal-box tracks-box"
-            style={{ zIndex: 1, transformOrigin: "bottom left" }}
-          >
-            <Image
-              src={img2}
-              alt="IOT"
-              width={80}
-              height={80}
-              className="icon"
-            />
-            <h3 className="text">{title2}</h3>
-          </motion.div>
-        </div>
-      )}
-    </AnimatePresence>
+      transition: {
+        duration: 0.3,
+        delay: 0.725,
+        type: "spring",
+        stiffness: 500,
+        damping: 30,
+        mass: 1,
+      },
+    },
+  };
+  return (
+    <div className="horizontal-container">
+      <motion.div
+        key="1"
+        variants={animation}
+        ref={ref}
+        initial={{ height: "0%" }}
+        animate={inView ? "visible" : ""}
+        className="horizontal-box tracks-box "
+        style={{ transformOrigin: " top left", zIndex: 3 }}
+      >
+        <Image src={img1} alt="IOT" width={80} height={80} className="icon" />
+        <h3 className="text">{title1}</h3>
+      </motion.div>
+      <motion.div
+        key="2"
+        variants={animation}
+        initial={{ y: "-220%", rotate: -10 }}
+        animate={inView ? "rotate" : ""}
+        className="horizontal-box tracks-box"
+        style={{ zIndex: 1, transformOrigin: "bottom left" }}
+      >
+        <Image src={img2} alt="IOT" width={80} height={80} className="icon" />
+        <h3 className="text">{title2}</h3>
+      </motion.div>
+    </div>
   );
 };
 export default HorizontalBox;
--- src/components/tracks/Tracks.jsx
+++ src/components/tracks/Tracks.jsx
@@ -2,45 +2,60 @@ import React, { useRef } from "react";
 import VerticalBox from "./VerticalBox";
 import HorizontalBox from "./HorizontalBox";
 import Heading from "../Heading";
+import Paragraph from "../Paragraph";
 import { useInView } from "framer-motion";
 
 const Tracks = () => {
   const ref = useRef(null);
   const inView = useInView(ref);
-  console.log(inView);
+  // console.log(inView);
   return (
     <section id="tracks" className="Track-section">
       <div className="heading">
         <h1 className="heading-shadow">TRACKS</h1>
         <Heading className="heading-small">TRACKS</Heading>
       </div>
+      <div className="tracks-para">
+        <Paragraph>
+          tracks HackTheSpace features a variety of tracks, each designed to
+          guide hackers to tackle specific challenges. By offering diverse
+          tracks, we cater to a wide range of interests and expertise, fostering
+          creativity, collaboration, and meaningful innovation among hackers.
+        </Paragraph>
+        <Paragraph>
+          HackTheSpace isn't just an event; it's a chance to Think, Build and
+          Launch. With diverse tracks covering cutting-edge tech, there's
+          something for everyone, from experts to newcomers. Join us on this
+          journey of discovery and problem-solving. Let's unleash tech's full
+          potential and make a lasting impact.
+        </Paragraph>
+      </div>
       <div className="tracks-container">
-        <div className="box-bundle">
-          <VerticalBox title={"Open Innovation"} img={"open.svg"} />
-
+        <div className="box-bundle" ref={ref}>
+          <HorizontalBox
+            title1={"Open Innovation"}
+            title2={"Security & Audits"}
+            inView={true}
+            img1={"open.svg"}
+            img2={"security.svg"}
+          />
           <HorizontalBox
             title1={"Generative AI & Machine Learning"}
-            title2={"Augmented & virtual reality"}
-            inView={inView}
+            title2={"Augumented & Vertual Reality "}
+            inView={true}
             img1={"AI - ML.svg"}
-            img2={"/VR.svg"}
+            img2={"VR.svg"}
           />
         </div>
-
         <div className="box-bundle" ref={ref}>
-          <VerticalBox title={"Security & Audits"} img={"security.svg"} />
-
           <HorizontalBox
             title1={"Internet Of Things"}
             title2={"Cloud & Devops"}
-            inView={inView}
+            inView={true}
             img1={"IOT.svg"}
             img2={"Cloud.svg"}
           />
-        </div>
-
-        <div className="box-bundle">
-          <VerticalBox title={"Open Innovation"} img={"open.svg"} />
+          <VerticalBox title={"Open Innovation"} img={"idea.svg"} />
         </div>
       </div>
     </section>
