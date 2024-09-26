455950049c5b3b4d3dd249bd0ee7d21d798f2ca9
girish sinha <128844352+girishsinha@users.noreply.github.com>
2024-09-27 01:02:51 +0530

    Display page (#52)

--- src/app/(display)/timeconfig.js
+++ src/app/(display)/timeconfig.js
@@ -1,116 +1,93 @@
 export const timeconfig = [
     {
         id: "1",
-        time: "8:30",
+        time: "7:45",
         period: "AM",
         content: "Check-in & Welcome Kit Distribution",
-        duration: "30"
     },
     {
         id: "2",
-        time: "9:00",
+        time: "8:30",
         period: "AM",
         content: "Light Breakfast & Ice Breaker",
-        duration: "60"
     },
     {
         id: "3",
         time: "10:00",
         period: "AM",
         content: "Opening Ceremony",
-        duration: "30"
     },
     {
         id: "4",
         time: "10:30",
         period: "AM",
         content: "Hacking Begins",
-        duration: "30"
     },
     {
         id: "5",
         time: "11:00",
         period: "AM",
         content: "Team Building Event",
-        duration: "120"
     },
     {
         id: "6",
-        time: "1:00",
+        time: "1:30",
         period: "PM",
         content: "Lunch",
-        duration: "150"
     },
     {
         id: "7",
         time: "3:30",
         period: "PM",
         content: "Speaker Session",
-        duration: "150"
     },
     {
         id: "8",
         time: "6:00",
         period: "PM",
         content: "Github Mini Event",
-        duration: "150"
     },
     {
         id: "9",
         time: "8:30",
         period: "PM",
         content: "Dinner",
-        duration: "60"
     },
     {
         id: "10",
         time: "9:30",
         period: "PM",
         content: "Mini Events",
-        duration: "630"
     },
     {
-        id: "11:",
+        id: "11",
         time: "8:00",
         period: "AM",
         content: "Breakfast",
-        duration: "150"
     },
     {
         id: "12",
         time: "10:30",
         period: "AM",
         content: "Hacking Ends",
-        duration: "150"
     },
     {
         id: "13",
-        time: "12:00",
-        period: "PM",
-        content: "Lunch",
-        duration: "30"
-    },
-    {
-        id: "14",
         time: "12:30",
         period: "PM",
         content: "Judging Starts",
-        duration: "135"
+    },
+    {
+        id: "14",
+        time: "2:00",
+        period: "PM",
+        content: "Lunch",
     },
     {
         id: "15",
-        time: "6:32",
+        time: "3:00",
         period: "PM",
         content: "Closing Ceremony",
-        duration: "0"
     },
-    {
-        id: "15",
-        time: "6:33",
-        period: "PM",
-        content: "testing Ceremony",
-        duration: "0"
-    },
-
 
 ];
