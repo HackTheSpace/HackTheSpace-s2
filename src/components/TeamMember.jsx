27d94224c0ca3acf2e4fca0626aa09940fe00e96
mohit makda <mohitmakda1404@gmail.com>
2024-10-02 23:07:22 +0530

    team

--- src/components/TeamMember.jsx
+++ src/components/TeamMember.jsx
@@ -1,21 +1,20 @@
 import React from "react";
-import Image from 'next/image'; // Importing Image component from Next.js
+import Image from 'next/image'; 
 import { FaLinkedin, FaGithub, FaInstagram, FaBehance } from "react-icons/fa";
-import { AiOutlineTwitter } from "react-icons/ai"; // X logo representation
-
+import { AiOutlineTwitter } from "react-icons/ai"; 
 const TeamMember = ({ name, role, role2, imageSrc, socials }) => {
   return (
     <div className="team-member">
       <div className="team-member-image bw-image">
-        {/* Using Next.js Image component */}
+      
         <Image
           src={imageSrc}
           alt={`${name}'s profile`}
-          width={150}  // Add desired width
-          height={150} // Add desired height
-          layout="responsive" // Ensures responsiveness
-          priority={true} // Optionally prioritize loading
-          className="profile-image" // Add custom class for styling if needed
+          width={150} 
+          height={150} 
+          layout="responsive" 
+          priority={true}
+          className="profile-image" 
         />
       </div>
       <div className="team-member-details">
