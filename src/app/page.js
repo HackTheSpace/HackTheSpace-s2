fcd905ef5db49508e33df240aa744bccfbe44472
Harsh Gupta <101264150+Harsh3341@users.noreply.github.com>
2024-11-21 00:03:27 +0530

    minor fix in naming

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import HeroSection from "@/components/HeroSection";
 import WhatIsHTS from "@/components/WhatIsHTS";
 import Glimpse from "@/components/Glimpse";
 import Organizers from "@/components/Organizers";
-import Judge from "@/components/judge";
+import Judge from "@/components/Judge";
 
 export default function Home() {
   return (
